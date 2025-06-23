b004e7405c416cadd407ca2c97fe5a5dbe563470
Siddharth0110 <jadhavsiddheshwar0110@gmail.com>
2025-06-24 00:45:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+    res.send('VCTC Backend is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
